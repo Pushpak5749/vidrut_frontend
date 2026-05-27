acc0ec4af194f3bad521204ef92c04faf28d1fed
Pushpak5749 <pushpakchanda5749@gmail.com>
2026-05-27 22:26:43 +0530

    Bind to 0.0.0.0

--- server.js
+++ server.js
@@ -73,6 +73,6 @@ app.use((req, res) => {
     res.status(404).send('Page not found');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
